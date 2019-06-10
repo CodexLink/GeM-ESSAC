9419a6a7f614fbefeec076eb6e55daa6a09d5ad3
Janrey Licas <janjan.licas@gmail.com>
2019-06-10 16:58:01 +0800

    [Prototype] Switch Integration Initial Function

---
+++ TwoModeSwitch/TwoModeSwitch.ino
@@ -0,0 +1,32 @@
+#define SwitchMode_One 26
+#define SwitchMode_Two 27
+
+#define INVERSE_LOW HIGH
+#define INVERSE_HIGH LOW
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(SwitchMode_One, INPUT_PULLUP);
+    pinMode(SwitchMode_Two, INPUT_PULLUP);
+}
+
+void loop()
+{
+    if ((digitalRead(SwitchMode_One) == INVERSE_HIGH) && (digitalRead(SwitchMode_Two) == INVERSE_LOW))
+    {
+        Serial.println("Mode 1 Switch");
+    }
+    else if ((digitalRead(SwitchMode_One) == INVERSE_HIGH) && (digitalRead(SwitchMode_Two) == INVERSE_HIGH))
+    {
+        Serial.println("Mode 2 Switch");
+    }
+    else if ((digitalRead(SwitchMode_One) == INVERSE_LOW) && (digitalRead(SwitchMode_Two) == INVERSE_HIGH))
+    {
+        Serial.println("Mode 3 Switch");
+    }
+    else if ((digitalRead(SwitchMode_One) == INVERSE_LOW) && (digitalRead(SwitchMode_Two) == INVERSE_LOW))
+    {
+        Serial.println("Mode 4 Switch");
+    }
+    delay(500);
+}
