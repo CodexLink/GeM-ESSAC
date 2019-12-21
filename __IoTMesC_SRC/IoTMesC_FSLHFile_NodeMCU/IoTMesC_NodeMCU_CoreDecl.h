9d811e8e7b389baadb7e53fc71c860b70c74db6e
Janrey Licas <janjan.licas@gmail.com>
2019-12-21 23:21:05 +0800

    NodeMCU Dev | Cleaned Header Related To AVR Clean

--- __IoTMesC_SRC/IoTMesC_FSLHFile_NodeMCU/IoTMesC_NodeMCU_CoreDecl.h
+++ __IoTMesC_SRC/IoTMesC_FSLHFile_NodeMCU/IoTMesC_NodeMCU_CoreDecl.h
@@ -7,128 +7,86 @@
 */
 #pragma once
 
+// ! Libraries |> Main Dependency
+#include "CmpntCore/ESP8266WiFi/ESP8266WiFi.h"
+#include "CmpntCore/ESP8266mDNS/ESP8266mDNS.h"
+#include "CmpntCore/ESP8266WebServer/ESP8266WebServer.h"
+//#include <WiFiClient.h>
 
-// Choices for using this library.
-//#define IOT_DEVICE_CONTROLLER_ESP_SERVICE_NET 0x41726475696e6f20415652
-//#define DEBUG_ENABLED
+// ! Libraries |> System Compiler Libraries Provided.
+#include <SPI.h>
 
 #ifdef DEBUG_ENABLED
-    #if Cmd_Func == println || print
-        #define SerialH_Call(Cmd_Func, Params) Serial.Cmd_Func(Params)
-        #define SerialR_Call(Cmd_Func, Params) Serial1.Cmd_Func(Params)
-    #else
-        #define SerialH_Call(Cmd_Func, Params)
-        #define SerialR_Call(Cmd_Func, Params)
-    #endif
+#if Cmd_Func == println || print
+#define SerialH_Call(Cmd_Func, Params) Serial.Cmd_Func(Params)
+#define SerialR_Call(Cmd_Func, Params) Serial1.Cmd_Func(Params)
+#else
+#define SerialH_Call(Cmd_Func, Params)
+#define SerialR_Call(Cmd_Func, Params)
+#endif
 #endif
 
+class IoTMesC_NMCU_DRVR
+{
+private:
+    typedef struct UserCredentials;
+    typedef struct WiFiCredentials;
+    enum ESP_ModeFlags;
+    static uint_fast8_t SET_HANDLE_FLAG;
 
-#if defined(IOT_DEVICE_CONTROLLER_AVR_DATASENS) && defined(IOT_DEVICE_CONTROLLER_ESP_SERVICE_NET)
-    #error "IoTMesC Core Initialization Halted. Both candidate devices are defined. Please define only one!"
-#endif
+    uint_fast8_t WIFI_DEFINED_COUNT;
 
-// * IOT_DEVICE_CONTROLLER_ESP_SERVICE_NET — Device Definition Declarations
-#if defined(IOT_DEVICE_CONTROLLER_ESP_SERVICE_NET)
-    #ifndef IOT_DEVICE_CONTROLLER_ESP_SERVICE_NET_GUARD
-       #define IOT_DEVICE_CONTROLLER_ESP_SERVICE_NET_GUARD
-    #endif
+    // * Initializers
+    void begin() const;
+    void init_ESPDev(ESP_PROPERTIES::ModeFlags FlagGiven);
+    void init_ESPWiFi();
+    // * setState FN Members
 
-    #ifndef IOT_ESP_GUARD
-        #define IOT_ESP_GUARD
+    // * WiFi Functionalities
+    void WiFi_setBroadcast() noexcept;
+    void WiFi_Broadcast() noexcept;
+    void WiFi_Enable() noexcept;
+    void WiFi_Disable() noexcept;
 
-            #include "CmpntCore/ESP8266WiFi/ESP8266WiFi.h"
-            #include "CmpntCore/ESP8266mDNS/ESP8266mDNS.h"
-            #include "CmpntCore/ESP8266WebServer/ESP8266WebServer.h"
-            //#include <WiFiClient.h>
+    // HTTP Request Handler
 
-    #endif
-
-    namespace IoTMesC_NMCU_DEV_DECL
-    {
-        class IoTMesC_NMCU_DRVR;
-
-        namespace ESP_PROPERTIES
-        {
-            enum ModeFlags
-            {
-                DISABLED_FLAG = -2,
-                ERROR_FLAG = -1,
-                SUCCESS_FLAG = 0,
-                IDLE_MODE = 1,
-                DEBUG_MODE = 2,
-                RECONATTMPT_MODE = 3,
-                RETRY_MODE = 4,
-                DEPENDENT_MODE = 5,
-                LOW_VOLT_MODE = 6,
-                EMERGENCY_MODE = 7,
-                RESET_FLAG = 8
-
-            };
-
-        }
-        /*
-        namespace MyNamespace
-        {
-            enum MyEnum
-            {
-
-            };
-        }
-        */
-    }
-
-    class IoTMesC_NMCU_DEV_DECL::IoTMesC_NMCU_DRVR
-    {
-
+    // Site Handler
     public:
-        // Constructor
-        IoTMesC_NMCU_DRVR(void) {}
-        // Destructor
-        ~IoTMesC_NMCU_DRVR(void) {}
+    // Constructor
+    IoTMesC_NMCU_DRVR(void) {}
+    // Destructor
+    ~IoTMesC_NMCU_DRVR(void) {}
 
-    private:
-        typedef struct UserCredentials;
-        typedef struct WiFiCredentials;
-        static uint_fast8_t SET_HANDLE_FLAG;
+};
 
-        uint_fast8_t WIFI_DEFINED_COUNT;
+// Struct Declarations
+typedef struct UserCredentials
+{
+    uint_fast16_t UserID;
+    char *Username;
+    char *Password;
+};
 
-        // * Initializers
-        void begin() const;
-        void init_ESPDev(ESP_PROPERTIES::ModeFlags FlagGiven);
-        void init_ESPWiFi();
-        // * setState FN Members
+typedef struct WiFiCredentials
+{
+    char *WiFi_SSID;
+    char *WiFi_Password;
+};
 
-
-        // * WiFi Functionalities
-        void WiFi_setBroadcast() noexcept;
-        void WiFi_Broadcast() noexcept;
-        void WiFi_Enable() noexcept;
-        void WiFi_Disable() noexcept;
-
-        // HTTP Request Handler
-
-        // Site Handler
-
-    };
-
-    // Struct Declarations
-    typedef struct UserCredentials
-    {
-        uint_fast16_t UserID;
-        char *Username;
-        char *Password;
-    };
-
-
-    typedef struct WiFiCredentials
-    {
-        char* WiFi_SSID;
-        char* WiFi_Password;
-    };
-
-#endif // * Definition End Point |> IOT_DEVICE_CONTROLLER_ESP_SERVICE_NET
-
+enum ESP_ModeFlags
+{
+    DISABLED_FLAG = -2,
+    ERROR_FLAG = -1,
+    SUCCESS_FLAG = 0,
+    IDLE_MODE = 1,
+    DEBUG_MODE = 2,
+    RECONATTMPT_MODE = 3,
+    RETRY_MODE = 4,
+    DEPENDENT_MODE = 5,
+    LOW_VOLT_MODE = 6,
+    EMERGENCY_MODE = 7,
+    RESET_FLAG = 8
+};
 /*
     Name: IoT Multi-Essential Sensing Components | For Personal Benefits
     Type: Library Core Header | Declarations Only For NodeMCU (ESP8266)
