c1914b16e69933040468ecd63d4a2240bd743010
Janrey Licas <janjan.licas@gmail.com>
2020-05-28 00:27:10 +0800

    GeMCore::TypeDecl | Init && FLSHF | Updated Path.

---
+++ SketchSRC/FLSHF_NodeMCU/Cores/GEMCore/GeMCore_TypeDecl.h
@@ -0,0 +1,23 @@
+/* =============================================================================
+!  Author:          Janrey "CodexLink" Licas | https://github.com/CodexLink
+*  Email:           janjan.licas@gmail.com
+##  FileName:        GeMCore_TypeDecl.h
+##  Description:     Extra Definitions for Type Shortened Data Types and Name References.
+!  Version:          0.0.1
+*  History:         05/27/2020 - First File Init
+============================================================================= */
+
+#pragma once
+
+#include <inttypes.h>
+
+// # Type Definitions
+typedef uint8_t byte_p;
+typedef int8_t byte_i;
+typedef int16_t int_s;
+typedef int64_t int_ll;
+typedef const struct struct_c;
+
+// # Constant Expressions at Compile Time
+constexpr byte_p FUNC_SUCCESS = 0;
+constexpr byte_p FUNC_FAILED = 69;
