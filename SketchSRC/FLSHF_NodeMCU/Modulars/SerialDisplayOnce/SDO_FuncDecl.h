aa7b2d43a38461cff2a3475bd17da2ab29becf62
Janrey Licas <janjan.licas@gmail.com>
2020-05-30 23:20:34 +0800

    SDO | Initially Create Class To Use + Virtuals

---
+++ SketchSRC/FLSHF_NodeMCU/Modulars/SerialDisplayOnce/SDO_FuncDecl.h
@@ -0,0 +1,42 @@
+/* =============================================================================
+!  Author:          Janrey "CodexLink" Licas | https://github.com/CodexLink
+*  Email:           janjan.licas@gmail.com
+##  FileName:        SDO_FuncDecl.h
+##  Description:     Function Declarations for Serial Display Data Once.
+!  Version:          0.0.1
+*  History:         05/16/2020 - First File Init
+============================================================================= */
+
+#pragma once
+
+/*
+# @technical
+
+! The main use of this library was to be used in instantiated way.
+* Each should have required functions that are declared as pure virtual functions.
+! For every instance, Each Name Will Be Different Base On Given Argument 1 from Constructor.
+*/
+
+#include <inttypes.h>
+
+class SerialDisplay
+{
+public:
+    SerialDisplay(char *LibraryUserName);
+
+private:
+    static constexpr uint8_t MAX_MESSAGE_CHAR = 255;
+    static constexpr uint8_t MAX_ERR_CODE = 6; // ! Example `CNT-01`.
+    struct SerialData
+    {
+        char currMessage[MAX_MESSAGE_CHAR];
+        char currErrCode[MAX_ERR_CODE];
+    };
+
+    // # Why Virtuals? We need to make those derived class an opportunity to create own style of strings.
+    void setErrorCode();
+    virtual void printErrorCode() = 0;
+    virtual void printMsg(char msgToDisplay) = 0; // ! Output Data Once When Data IS Almost The Same as Stored Here.
+
+    virtual void forceDataDisplay(); // ! Forces To Display Messages Added
+}
---
+++ SketchSRC/FLSHF_NodeMCU/Modulars/SerialDisplayOnce/SerialDisplayOnce.ino
@@ -0,0 +1,11 @@
+// ! Create Header For Each Sketch
+
+#include "SDO_FuncDecl.h"
+
+void setup()
+{
+}
+
+void loop()
+{
+}
